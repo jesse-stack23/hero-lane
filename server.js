421d14287c3936e02d7b17ee58c08d2e04a0c7a1
jesse-stack23 <90689683+jesse-stack23@users.noreply.github.com>
2021-10-07 12:36:39 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,81 @@
+const {client} = require('pg');
+var express = require('express');
+var bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const client = new Client({
+	connectionString: "postgres://fertcugcyczjpp:0458fa8a42c4a15c3a832b5f57633ee4afe1439e1031c51026fac9dcd6c566f1@ec2-54-167-152-185.compute-1.amazonaws.com:5432/d5fgkbi00p3hr4" ,
+	ssl: true,
+});
+
+client.connect();
+
+var myapp = express();
+const path = require('path');
+const router = express.Router();
+
+myapp.use(function(req, res, next){
+	req.headers['content-type'] = "application/json";
+	next();
+});
+
+myapp.get('/' function(req, res){
+	res.sendFile(__hero-lane);
+	res.sendFile(path:join(__hero-lane + '/UI/index.html'));
+});
+
+myapp.use(express.static(__hero-lane + '/UI'));
+myapp.use(bodyParser.urlencoded({extended: true}));
+myapp.use(bodyParser.json());
+
+myapp.post('/auth/signup', function(req, res){
+	client.connect();
+	
+	var datae = {};
+	var user = {};
+	var mamail = req.body.email;
+	var mauser_name = req.body.user_name;
+	var mateam_name = req.body.team_name;
+	var mapassword = req.body.Password;
+	var maId = 3;
+	
+user['email'] = mamail;
+user['password'] = mapassword;
+jwt.sign(user, mapassword, {expiresIn: '/h'},(errt, token) => {
+	
+	if(errt){
+		datae['status'] = 404;
+		datae['error'] = "Error: Connection Not Secure...";
+		res.send(datae);
+	}else{
+		client.query("SELECT id FROM users ORDER BY id DESC;" ,(errf, respf)=>{
+			if(errf){
+				
+			}else{
+				var newid = respf.rows[0].id + 1;
+				const text = "INSERT INTO users(id, user_name, team_name, password, email) VALUES ('" + newId +"', '"+ mauser_name +"', '"+ mateam_name +"', '"+ mapassword +"', '"+ mamail + "') RETURNING id; ";
+				
+				client.query(text,(err, resp) => {
+					if(err){
+						datae['status'] = 404;
+						datae['error'] = "Error: Problem occured when signing up...";
+						res.send(datae);
+					}else{
+						
+						datae['status'] = 200;
+						var arr = {};
+						arr['id'] = resp.rows[0].id;
+						arr['user_name'] = mauser_name;
+						arr['team_name'] = mateam_name;
+						arr['email'] = mamail;
+						arr['password'] = mapassword;
+						datae['data'] = arr;
+						res.send(datae);
+					}
+				});
+			}
+		});		
+	}
+});
+});
+const portr = process.env.PORT || 3000;
+myapp.listen(portr);
